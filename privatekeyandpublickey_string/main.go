cfff3bb3c83f9ad5c10289063f721a412dc9d07c
Federico Pereira <lord.basex@gmail.com>
2022-08-13 16:52:42 -0300

    Create main.go

---
+++ privatekeyandpublickey_string/main.go
@@ -0,0 +1,46 @@
+/*
+ * Genarate rsa keys.
+ */
+
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2014)
+	if err != nil {
+		log.Print(err)
+	}
+
+	privateKeyDer := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privateKeyDer,
+	}
+	privateKeyPem := string(pem.EncodeToMemory(&privateKeyBlock))
+
+	publicKey := privateKey.PublicKey
+	publicKeyDer, err := x509.MarshalPKIXPublicKey(&publicKey)
+	if err != nil {
+		log.Print(err)
+	}
+
+	publicKeyBlock := pem.Block{
+		Type:    "PUBLIC KEY",
+		Headers: nil,
+		Bytes:   publicKeyDer,
+	}
+	publicKeyPem := string(pem.EncodeToMemory(&publicKeyBlock))
+
+	fmt.Println(privateKeyPem)
+	fmt.Println(publicKeyPem)
+}
