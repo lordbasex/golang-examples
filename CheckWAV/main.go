08b1761ca30d05e9f032dd58bc042e2d44659f4f
Federico Pereira <lord.basex@gmail.com>
2023-04-04 07:38:00 -0300

    Update main.go

--- CheckWAV/main.go
+++ CheckWAV/main.go
@@ -1,58 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"os"
+        "fmt"
+        "log"
+        "os"
+        "strings"
 
-	"github.com/youpy/go-wav"
+        "github.com/go-audio/wav"
 )
-
-func read_wav_file(input_file string, number_of_samples uint32) ([]float64, uint16, uint32, uint16) {
-
-	if number_of_samples == 0 {
-		number_of_samples = math.MaxInt32
-	}
-
-	blockAlign := 2
-	file, err := os.Open(input_file)
-	if err != nil {
-		panic(err)
-	}
-
-	reader := wav.NewReader(file)
-	wavformat, err_rd := reader.Format()
-	if err_rd != nil {
-		panic(err_rd)
-	}
-
-	if wavformat.AudioFormat != wav.AudioFormatPCM {
-		panic("Audio format is invalid ")
-	}
-
-	if int(wavformat.BlockAlign) != blockAlign {
-		fmt.Println("Block align is invalid ", wavformat.BlockAlign)
-	}
-
-	samples, err := reader.ReadSamples(number_of_samples)
-
-	wav_samples := make([]float64, 0)
-
-	for _, curr_sample := range samples {
-		wav_samples = append(wav_samples, reader.FloatValue(curr_sample, 0))
-	}
-
-	return wav_samples, wavformat.BitsPerSample, wavformat.SampleRate, wavformat.NumChannels
-}
 
 func main() {
 
-	input_audio := "demo1.wav"
+        f, err := os.Open("demo.wav")
+        if err != nil {
+                panic(fmt.Sprintf("couldn't open audio file - %v", err))
+        }
 
-	audio_samples, bits_per_sample, input_audio_sample_rate, num_channels := read_wav_file(input_audio, 0)
+        d := wav.NewDecoder(f)
+        _, err = d.FullPCMBuffer()
 
-	fmt.Println("num samples ", len(audio_samples)/int(num_channels))
-	fmt.Println("bit depth   ", bits_per_sample)
-	fmt.Println("sample rate ", input_audio_sample_rate)
-	fmt.Println("num channels", num_channels)
+        if err != nil {
+                panic(err)
+        }
+
+        f.Close()
+
+        typeFileTmp := fmt.Sprintf("%v", d)
+        substrStart := strings.Index(typeFileTmp, "WAVE")
+        typeFile := string([]rune(typeFileTmp)[substrStart : substrStart+4])
+
+        log.Printf("TypeFile: %v", typeFile)
+        log.Printf("BitDepth: %v", d.BitDepth)
+        log.Printf("SampleRate: %v", d.SampleRate)
+        log.Printf("Channels: %v", d.NumChans)
+
 }
